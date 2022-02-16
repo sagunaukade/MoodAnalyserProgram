8f649523d3920d2cab17627e69b45edaffc5ba6a
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-16 22:50:15 +0530

    Update MoodAnalyserTest.cs

--- MoodAnalyser/MoodAnalyseTest/MoodAnalyserTest.cs
+++ MoodAnalyser/MoodAnalyseTest/MoodAnalyserTest.cs
@@ -1,14 +1,43 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using MoodAnalyser;
 
 namespace MoodAnalyseTest
 {
     [TestClass]
-    public class UnitTest1
+    public class MoodAnalyserTest
     {
         [TestMethod]
-        public void TestMethod1()
+        [TestCategory("Sad Message")]
+        public void TestSadMoodInMessage()
         {
+            //Arrange
+            string message = "I am in sad Mood";
+            string expected = "SAD";
+            MoodAnalyser moodAnalyzer = new MoodAnalyser(message);
+
+            //Act
+            string actual = moodAnalyzer.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        [TestCategory("Happy Message")]
+        public void TestHappyMoodInMessage()
+        {
+            //Arrange
+            string message = "I am in Any Mood";
+            string expected = "HAPPY";
+            MoodAnalyser moodAnalyzer = new MoodAnalyser(message);
+
+            //Act
+            string actual = moodAnalyzer.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
