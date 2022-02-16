e113222096639b8beb8c67db23fc391f56e921b3
sagunaukade <sagunaukade96@gmail.com>
2022-02-16 20:36:03 +0530

    [Saguna] Added program to mood analyse

---
+++ MoodAnalyser/MoodAnalyseTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoodAnalyseTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoodAnalyseTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1288e2d-1856-47ce-b94e-ef0d76852d35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MoodAnalyser/MoodAnalyser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   // display welcome message
+            Console.WriteLine("Welcome to the mood analyser programs");
+
+            //Creating MoodAnalyzer object usecase-1
+            MoodAnalyse moodAnalyser = new MoodAnalyse("I am in Happy Mood");
+            Console.WriteLine("Mood is: " + moodAnalyser.AnalyseMood());
+            Console.ReadLine();
+        }
+    }
+}
