6a6f9b1fdf63dcdd074c3bdd9affe3aa36f1afb5
sagunaukade <sagunaukade96@gmail.com>
2022-02-16 23:00:06 +0530

    [Saguna] Added program to Exception handling

--- MoodAnalyser/MoodAnalyseTest/MoodAnalyserTest.cs
+++ MoodAnalyser/MoodAnalyseTest/MoodAnalyserTest.cs
@@ -1,14 +1,61 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using MoodAnalyser;
 
 namespace MoodAnalyseTest
 {
     [TestClass]
-    public class UnitTest1
+    public class MoodAnalyserTest
     {
+        //Method to test Sad Mood
         [TestMethod]
-        public void TestMethod1()
+        [TestCategory("Sad Message")]
+        public void TestSadMoodInMessage()
         {
+            //Arrange
+            string message = "I am in sad Mood";
+            string expected = "SAD";
+            MoodAnalyse moodAnalyzer = new MoodAnalyse(message);
+
+            //Act
+            string actual = moodAnalyzer.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Method to test Happy Mood
+        [TestMethod]
+        [TestCategory("Happy Message")]
+        public void TestHappyMoodInMessage()
+        {
+            //Arrange
+            string message = "I am in Any Mood";
+            string expected = "HAPPY";
+            MoodAnalyse moodAnalyzer = new MoodAnalyse(message);
+
+            //Act
+            string actual = moodAnalyzer.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Method to test Happy Mood in null message
+        [TestMethod]
+        [TestCategory("Null Exception")]
+        public void GivenNullMessageReturnHappyMood()
+        {
+            //Arrange
+            string message = null;
+            string expected = "HAPPY";
+            MoodAnalyse moodAnalyzer = new MoodAnalyse(message);
+
+            //Act
+            string actual = moodAnalyzer.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
