61d552d308350674e5f493f1bf13f8a9a2d38a6c
sagunaukade <sagunaukade96@gmail.com>
2022-02-17 15:03:17 +0530

    [Saguna] MoodAnalyser refactor

--- MoodAnalyser/MoodAnalyseTest/MoodAnalyserTest.cs
+++ MoodAnalyser/MoodAnalyseTest/MoodAnalyserTest.cs
@@ -7,37 +7,139 @@ namespace MoodAnalyseTest
     [TestClass]
     public class MoodAnalyserTest
     {
-       [TestMethod]
-        [TestCategory ("Sad Message")]
-        public void TestSadMoodInMessage()
+       MoodAnalyserReflector reflector;
+        [TestInitialize]
+        public void Setup()
         {
-            //Arrange
-            string message = "I am in sad Mood";
-            string expected = "SAD";
+            reflector = new MoodAnalyserReflector();
+        }
+        //TC 1.1,1.2,2.1 - Method to test Sad Mood and Happy Mood
+        [TestMethod]       
+        [TestCategory ("Test Mood in Message")]
+        //Arrange
+        [DataRow("I am in sad Mood", "SAD")]
+        [DataRow ("I am in Any Mood", "HAPPY")]
+        [DataRow(null, "HAPPY")]
+        public void TestMoodInMessage(string message, string expected)
+        {
             MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
-
             //Act
             string actual = moodAnalyzer.AnalyseMood();
-
             //Assert
             Assert.AreEqual(expected, actual);
         }
-
+        //TC 3.1 - Method to test Custom exception for null message
         [TestMethod]
-        [TestCategory("Happy Message")]
-        public void TestHappyMoodInMessage()
+        [TestCategory("Custom Exception")]
+        [DataRow (null, "Message should not be null")]
+        [DataRow ("", "Message should not be empty")]
+        public void GivenNullMessageTestCustomException(string userInput, string expected)
         {
-            //Arrange
-            string message = "I am in Any Mood";
-            string expected = "HAPPY";
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
-
-            //Act
-            string actual = moodAnalyzer.AnalyseMood();
-
-            //Assert
-            Assert.AreEqual(expected, actual);
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(userInput);
+            try
+            {
+                string actual = moodAnalyzer.AnalyseMood();
+            }
+            catch(MoodAnalyzerException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
         }
-    }
+        //TC 4.1 - Proper class details are provided and expected to return the MoodAnalyser Object
+        [TestMethod]
+        [TestCategory("Reflection")]
+        [DataRow("MoodAnalyzerProblem.Reflection.Customer", "Customer")]
+        public void GivenMoodAnalyzerClassName_ReturnMoodAnalyzerObject(string className, string constructorName)
+        {
+            MoodAnalyzer expected = new MoodAnalyzer();
+            object obj = null;
+
+            obj = reflector.CreateMoodMoodAnalyse(className, constructorName);
+            expected.Equals(obj);
+        }
+        //TC 4.2, 4.3 - improper class, constructor details are provided and expected to throw exception Class not found
+        [TestMethod]
+        [TestCategory("Reflection")]
+        [DataRow("MoodAnalyzerProblem.Reflection.Owner", "Reflection.Owner", "Class not found")]
+        [DataRow("MoodAnalyzerProblem.Reflection.Customer", "Reflection.OwnerMood", "Constructor not found")]
+        public void GivenImproperClassName_ThrowException(string className, string constructorName, string expected)
+        {
+            try
+            {
+                object actual = reflector.CreateMoodMoodAnalyse(className, constructorName);
+            }
+            catch (MoodAnalyzerException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+        //TC 5.1 - Method to test moodanalyser class with parameter constructor to check if two objects are equal
+        [TestCategory("Reflection")]
+        [TestMethod]
+        [DataRow("I am in Happy mood")]
+        [DataRow("I am in Sad mood")]
+        [DataRow("I am in any mood")]
+        public void GivenMessageReturnParameterizedConstructor(string message)
+        {
+            MoodAnalyzer expected = new MoodAnalyzer(message);
+            object obj = null;
+            try
+            {
+                obj = reflector.CreateMoodMoodAnalyserParameterObject("MoodAnalyzer", "MoodAnalyzer", message);
+            }
+            catch (MoodAnalyzerException actual)
+            {
+                Assert.AreEqual(expected, actual.Message);
+            }
+            obj.Equals(expected);
+        }
+        //TC 5.2 - Method to test moodanalyser with diff class with parameter constructor to throw error
+        [TestCategory("Reflection")]
+        [TestMethod]
+        [DataRow("Company", "I am in Happy mood", "Could not find class")]
+        [DataRow("Student", "I am in Sad mood", "Could not find class")]
+        public void GivenMessageReturnParameterizedClassNotFound(string className, string message, string expextedError)
+        {
+            try
+            {
+                object obj = reflector.CreateMoodMoodAnalyserParameterObject(className, "MoodAnalyzer", message);
+            }
+            catch (MoodAnalyzerException actual)
+            {
+                Assert.AreEqual(expextedError, actual.Message);
+            }
+        }
+        //TC 5.3 - Method to test moodanalyser with diff constructor with parameter constructor to throw error
+        [TestCategory("Reflection")]
+        [TestMethod]
+        [DataRow("Customer", "I am in Happy mood", "Could not find constructor")]
+        [DataRow("Student", "I am in Sad mood", "Could not find constructor")]
+        public void GivenMessageReturnParameterizedConstructorNotFound(string constructor, string message, string expextedError)
+        {
+            try
+            {
+                object obj = reflector.CreateMoodMoodAnalyserParameterObject("MoodAnalyzer", constructor, message);
+            }
+            catch (MoodAnalyzerException actual)
+            {
+                Assert.AreEqual(expextedError, actual.Message);
+            }
+        }
+        //UC 6.1,6.2 - Method to invoke analyse mood method to return happy or sad or invalid method
+        [TestCategory("Reflection")]
+        [TestMethod]
+        [DataRow("HAPPY")]
+        [DataRow("Method not found")]
+        public void ReflectionReturnMethod(string expected)
+        {
+            try
+            {
+                string actual = reflector.InvokeMoodAnalyzer("happy", "AnalyseMood");
+            }
+            catch(MoodAnalyzerException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+     }
 }
-   
